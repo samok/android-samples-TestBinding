7b3408795006c6ea4b2d18a6abe97c734bafa130
Florent Bazin <florent.bazin1@gmail.com>
2019-01-18 15:39:31 +0100

    init commit.

---
+++ app/src/main/java/component/initiative/com/testbinding/MainActivityVM.java
@@ -0,0 +1,25 @@
+package component.initiative.com.testbinding;
+
+/**
+ * Created by florent on 14/04/2017.
+ */
+public class MainActivityVM {
+private String nom;
+  private String prenom;
+
+  public String getNom() {
+	return nom;
+  }
+
+  public void setNom(String nom) {
+	this.nom = nom;
+  }
+
+  public String getPrenom() {
+	return prenom;
+  }
+
+  public void setPrenom(String prenom) {
+	this.prenom = prenom;
+  }
+}
